cafa4188e0cc3e23c1ad694ddaba303cdc4cfc48
Fabián Católico <83664068+Fabiancas007@users.noreply.github.com>
2024-10-19 16:55:42 -0500

    Cambio de la ruta de deploy debido al react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/theRocks">
       <App />
     </BrowserRouter>
   </React.StrictMode>
